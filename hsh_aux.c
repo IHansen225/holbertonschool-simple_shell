1ef9a59e84622f52392d3aac6c70ed7a58fc47f2
gasstin <gastonl1994@gmail.com>
2022-08-04 13:10:26 -0700

    fixing exit

--- hsh_aux.c
+++ hsh_aux.c
@@ -95,7 +95,8 @@ char *check_existance(char *paths[], char *name, char *programname, int *pcp, in
 		while (name[j++])
                         ;
 		write(2, name, j);
-                write(2, ": not found\n", 10); /* cambiar por 11 */
+                write(2, ": not found", 11); /* cambiar por 11 */
+		write(2, "\n", 1);
                 return (NULL);
         }
         else
